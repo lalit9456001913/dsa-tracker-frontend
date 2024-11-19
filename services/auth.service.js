a2771f8a10b953647fbf3711263d4d2d6752e04b
lalit <lk945600@gmail.com>
2024-11-19 18:53:37 +0530

    auth login url issue

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+const ApiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 export const loginUser = async (credentials) => {
     try {
-        const response = await axios.post('http://localhost:5000/api/auth/login', credentials);
+        const response = await axios.post(`${ApiUrl}/auth/login`, credentials);
         return response.data; 
     } catch (error) {
         console.error('Error on Login:', error);
